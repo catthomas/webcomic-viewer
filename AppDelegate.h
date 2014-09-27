c92588b0ef2270376110c940662296f5c87825c9
catthomas <catstarrthomas@gmail.com>
2014-09-27 14:03:06 -0500

    Initial commit

---
+++ AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Webcomic-Viewer
+//
+//  Created by meow on 9/27/14.
+//  Copyright (c) 2014 catthomas. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Webcomic-Viewer
+//
+//  Created by meow on 9/27/14.
+//  Copyright (c) 2014 catthomas. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
